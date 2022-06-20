ced11b71a5d3e2b3ad2e5b585015dee78334b66c
IvanShmytov <ivanshmytov@mail.ru>
2022-06-20 18:23:14 +0400

    Добавьте файлы проекта.

---
+++ Calculator.cs
@@ -0,0 +1,26 @@
+﻿namespace _16._7._1._HW_03_
+{
+    public class Calculator
+    {
+        public int Additional(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Miltiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16._7._1._HW_03_
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

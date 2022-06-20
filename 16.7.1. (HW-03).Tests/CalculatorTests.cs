450e9b31b5408f108b08e3cbd14e3682b06915ab
IvanShmytov <100521162+IvanShmytov@users.noreply.github.com>
2022-06-20 18:30:59 +0400

    Add files via upload

---
+++ 16.7.1. (HW-03).Tests/CalculatorTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace _16._7._1._HW_03_.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalMustReturnCorrectNumber() 
+        {
+            var calc = new Calculator();
+            int numb = calc.Additional(3, 2);
+            Assert.AreEqual(numb, 5);
+        }
+        [Test]
+        public void SubtractioтMustReturnCorrectNumber()
+        {
+            var calc = new Calculator();
+            int numb = calc.Subtraction(3, 2);
+            Assert.AreEqual(numb, 1);
+        }
+        [Test]
+        public void MultiplicationMustReturnCorrectNumber()
+        {
+            var calc = new Calculator();
+            int numb = calc.Miltiplication(3, 2);
+            Assert.AreEqual(numb, 6);
+        }
+        [Test]
+        public void DivisionMustReturnCorrectNumber()
+        {
+            var calc = new Calculator();
+            int numb = calc.Division(30, 2);
+            Assert.AreEqual(numb, 15);
+        }
+        [Test]
+        public void MultiplicationMustThrowException()
+        {
+            var calc = new Calculator();
+            Assert.Throws<DivideByZeroException>(delegate { calc.Division(3, 0); });
+        }
+    }
+}
---
+++ 16.7.1. (HW-03).Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("16.7.1. (HW-03).Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("16.7.1. (HW-03).Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("15dc4ba2-03a4-44f6-bac5-73e4a77b951c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
